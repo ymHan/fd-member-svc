4e1770ab817fd5c9142cafc886bc2290049ecaf0
ymHan <onyxsard@gmail.com>
2023-12-11 10:30:28 +0900

    commit

---
+++ src/member/member.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Inject } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { SignInRequestDto, SignUpRequestDto, ValidateRequestDto } from './member.dto';
+import { MEMBER_SERVICE_NAME, SignUpResponse, SignInResponse, ValidateResponse } from './member.pb';
+import { MemberService } from './service/member.service';
+
+@Controller()
+export class MemberController {
+  @Inject(MemberService)
+  private readonly service: MemberService;
+
+  @GrpcMethod(MEMBER_SERVICE_NAME, 'SignUp')
+  private signup(payload: SignUpRequestDto): Promise<SignUpResponse> {
+    return this.service.signup(payload);
+  }
+
+  @GrpcMethod(MEMBER_SERVICE_NAME, 'SignIn')
+  private signin(payload: SignInRequestDto): Promise<SignInResponse> {
+    return this.service.signin(payload);
+  }
+
+  @GrpcMethod(MEMBER_SERVICE_NAME, 'Validate')
+  private validate(payload: ValidateRequestDto): Promise<ValidateResponse> {
+    return this.service.validate(payload);
+  }
+}
