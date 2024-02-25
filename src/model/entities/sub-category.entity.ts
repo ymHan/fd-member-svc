9264a4976e52171618f1354a649006a9039c1ef6
ymHan <onyxsard@gmail.com>
2024-02-25 18:29:58 +0900

    [ymhan]

--- src/model/entities/sub-category.entity.ts
+++ src/model/entities/sub-category.entity.ts
@@ -14,6 +14,9 @@ export class SubCategory extends BaseEntity {
   @Column()
   description: string; // 카테고리 설명
 
+  @Column({ nullable: true })
+  sort: number; // 카테고리 정렬
+
   @CreateDateColumn({ type: 'timestamp without time zone', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
 }
