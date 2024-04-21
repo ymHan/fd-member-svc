7156e016ec1af2264ead57ba2eae82ea1f01d1e4
ymHan <onyxsard@gmail.com>
2024-04-22 07:33:40 +0900

    [ymhan]

--- src/model/view/view-video.entity.ts
+++ src/model/view/view-video.entity.ts
@@ -2,45 +2,43 @@ import { ViewEntity, ViewColumn } from 'typeorm';
 
 @ViewEntity({
   expression: `
-    select
-        "ve"."id" as "id",
-        "ve"."userId",
-        "uae"."email",
-        "ve"."title",
-        "ve"."sub_title" as "subTitle",
-        "ve"."description",
-        "uae"."name" as "ownerName",
-        "uae"."nickname" as "ownerNickName",
-        cae."channelName" as "ownerChannelName",
-        concat("ve"."url", "upae"."photo") as "ownerProfileIconUrl",
-        make_urls("ve"."id", 'thumb'::bpchar) as thumbnailUrl,
-        "ve"."view_count" as "viewCount",
-        "ve"."reportCount",
-        "ve"."like_count" as "likesCount",
-        "ve"."duration",
-        get_categorysub_name("ve"."category") as "category",
-        get_categorysub_name("ve"."categorySub") as "categorySub",
-        "ve"."categorySubCode",
-        "ve"."recordType",
-        make_urls("ve"."id", 'file'::bpchar) as "contentUrlList",
-        make_urls("ve"."id", 'pose'::bpchar) as "poseIndicatorList",
-        ve."channelList",
-        ve."isPublic",
-        ve."isDeleted",
-        ve."nodeId",
-        ve."createdAt",
-        ve."updatedAt"
-    from "video_entity" "ve"
-             left join "user_account_entity" "uae" on "ve"."userId" = "uae"."id"
-             left join "user_profile_account_entity" "upae" on "uae"."profileId" = "upae"."id"
-             left join "channel_account_entity" "cae" on "cae"."id" = "uae"."channelId"
-    where
-        "ve"."isStatus" = true
+      SELECT ve.id AS id,
+             ve."userId" as userId,
+             uae.email as email,
+             ve.title as title,
+             ve.sub_title AS subTitle,
+             ve.description as description,
+             uae.name AS ownerName,
+             uae.nickname AS ownerNickName,
+             cae."channelName" AS ownerChannelName,
+             concat(ve.url, upae.photo) AS ownerProfileIconUrl,
+             make_urls(ve.id, 'thumb'::bpchar) AS thumbnailUrl,
+             ve.view_count AS viewCount,
+             ve."reportCount" as reportCount,
+             ve.like_count AS likesCount,
+             ve.duration as duration,
+             get_categorysub_name(ve.category) AS category,
+             get_categorysub_name(ve."categorySub") AS "categorySub",
+             ve."categorySubCode" as categorySubCode,
+             ve."recordType" as recordType,
+             make_urls(ve.id, 'file'::bpchar) AS "contentUrlList",
+             make_urls(ve.id, 'pose'::bpchar) AS "poseIndicatorList",
+             ve."channelList",
+             ve."isPublic",
+             ve."isDeleted",
+             ve."nodeId",
+             ve."createdAt",
+             ve."updatedAt"
+      FROM video_entity ve
+               LEFT JOIN user_account_entity uae ON ve."userId" = uae.id
+               LEFT OUTER JOIN user_profile_account_entity upae ON uae."profileId" = upae.id
+               LEFT OUTER JOIN channel_account_entity cae ON cae.id = uae."channelId"
+      WHERE ve."isStatus" = true
   `,
 })
 export class ViewVideo {
   @ViewColumn()
-  videoId: number;
+  id: number;
 
   @ViewColumn()
   userId: number;
@@ -104,6 +102,12 @@ export class ViewVideo {
 
   @ViewColumn()
   channelList: string[];
+
+  @ViewColumn()
+  isPublic: boolean;
+
+  @ViewColumn()
+  isDeleted: boolean;
 
   @ViewColumn()
   nodeId: string;
